112554a5ab61699813947b0783c3fddc8897b6af
Expert Snippet <g12360337@gmail.com>
2026-02-17 02:55:18 +0530

    Sellerdashboard Hidden

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -22,7 +22,7 @@ export const AppContextProvider = (props) => {
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
