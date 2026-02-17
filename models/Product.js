9768284b828288e8c945dd8afc73e40e889a15be
Expert Snippet <g12360337@gmail.com>
2026-02-17 13:03:29 +0530

    mongoose updated

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require("mongoose");
+import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema({
     userId: {type: String, required: true, ref: "user"},
